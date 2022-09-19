ae421e2f03bfa5e9aecb9f056b04db003ac44b1d
Tsov3sb5nat <balintabel89@gmail.com>
2022-09-19 15:36:12 +0200

    NELEMNTS helyett N_ELEMENTS-el kell használni.'' helyett

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
     for (int i = 0;)
     {
         b[i] = i * 2;
@@ -13,7 +13,7 @@ int main()
     for (int i = 0; i; i++)
     {
         std::cout << "Ertek:"
-    }    
+    }
     std::cout << "Atlag szamitasa: " << std::endl;
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++)
